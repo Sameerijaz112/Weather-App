69a3f591ba46e9162f045d35a8069d69384d2139
Sameer <sameerejaz970@gmail.com>
2022-12-09 10:13:38 +0500

    kkk

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from './enviroment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private  http:HttpClient) { }
+  getweather(cityName:string){
+    this.http.get(environment.weatherapiBAseURL), {
+      header: new HttpHeaders()
+      .set(environment.XrapidHostNAme, environment.XrapidHostValue)
+      .set(environment.XrapidKeyHostNAme, environment.XrapidKeyHosValue),
+      params: new HttpParams()
+      .set('q', cityName)
+      .set('units','metric')
+      .set('mode', 'json ')
+   
+    }
+  }
 }
